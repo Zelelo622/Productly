6adef29f11ce4556156f6e33e15b2706dbdab37b
Egor Kirin <egorcom27@gmail.com>
2022-06-10 23:05:22 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
-$(function(){
+$(function () {
     $('.slider').slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
         arrows: false,
-        dots: true
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1175,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
     });
 
-    $('.menu__burger').on('click', function(){
+    $('.menu__burger').on('click', function () {
         $('.menu__list').toggleClass('menu__list--actives')
     });
 });
